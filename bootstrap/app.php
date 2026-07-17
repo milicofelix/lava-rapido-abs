fe9db246444f5199fbd38efe0a8f77bd184eea09
Adriano Freitas <milicofelix@gmail.com>
2026-07-17 14:51:27 -0300

    chore: altera diretorio publico para public_html

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,21 +1,21 @@
 <?php
 
-use App\Http\Middleware\EnsureActiveSubscription;
-use App\Http\Middleware\RequestId;
-use App\Http\Middleware\SecurityHeaders;
 use App\Console\Commands\BackupCheckCommand;
-use App\Http\Middleware\EnsureUserCanAccess;
-use App\Http\Middleware\EnsureUserHasRole;
-use App\Http\Middleware\HandleInertiaRequests;
 use App\Console\Commands\ExpireLoyaltyCouponsCommand;
 use App\Console\Commands\ProductionCheckCommand;
 use App\Console\Commands\ReadinessCheckCommand;
+use App\Http\Middleware\EnsureActiveSubscription;
+use App\Http\Middleware\EnsureUserCanAccess;
+use App\Http\Middleware\EnsureUserHasRole;
+use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\RequestId;
+use App\Http\Middleware\SecurityHeaders;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Console\Scheduling\Schedule;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -52,4 +52,9 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
+
+$app->usePublicPath(base_path('public_html'));
+
+return $app;
