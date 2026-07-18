c7b31a0224a348f4f1f42ba4c80c275f931e497d
Adriano Freitas <milicofelix@gmail.com>
2026-07-18 12:14:03 -0300

    chore: ajustes em confis para socket

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,12 +8,25 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.Pusher = Pusher;
 
+const privateHostPattern = /^(localhost|127\.|10\.|192\.168\.|172\.(1[6-9]|2\d|3[01])\.)/;
+const configuredReverbHost = import.meta.env.VITE_REVERB_HOST;
+const currentHost = window.location.hostname;
+const currentScheme = window.location.protocol === 'https:' ? 'https' : 'http';
+const shouldUseConfiguredHost = configuredReverbHost
+    && ! (privateHostPattern.test(configuredReverbHost) && ! privateHostPattern.test(currentHost));
+const reverbScheme = shouldUseConfiguredHost
+    ? (import.meta.env.VITE_REVERB_SCHEME ?? currentScheme)
+    : currentScheme;
+const reverbPort = shouldUseConfiguredHost
+    ? (import.meta.env.VITE_REVERB_PORT ?? (reverbScheme === 'https' ? 443 : 80))
+    : (reverbScheme === 'https' ? 443 : 80);
+
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
+    wsHost: shouldUseConfiguredHost ? configuredReverbHost : currentHost,
+    wsPort: reverbPort,
+    wssPort: reverbPort,
+    forceTLS: reverbScheme === 'https',
     enabledTransports: ['ws', 'wss'],
 });
